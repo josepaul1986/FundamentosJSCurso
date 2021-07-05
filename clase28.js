fd6d32d1e3045d9676c8e4b8a74a30638109824c
Paul Alvarado <josepaul_ar@outlook.com>
2021-07-05 15:15:09 -0600

    Se agregan los archivos de clases practicas hechos por mi.

--- clase28.js
+++ clase28.js
@@ -1,10 +1,10 @@
-const API_URL = 'https://swapi.co/api/'
+const API_URL = 'https://swapi.dev/api/'
 const PEOPLE_URL = 'people/:id'
 
 
 const lukeUrl = `${API_URL}${PEOPLE_URL.replace(':id', 1)}`
 const opts = { crossDomain: true }
-
+//persona = data (ajax parameter)
 const onPeopleResponse = function (persona) {
   console.log(`Hola yo soy, ${persona.name}`)
 }
